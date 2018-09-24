9ca9e1d423abde94f16a0c470a38e201b37b1b12
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-09-24 17:25:26 +0200

    Added loading of partres files

---
+++ transphire_transform/dump_load/cter.py
@@ -0,0 +1,114 @@
+"""
+MIT License
+
+Copyright (c) 2018 Max Planck Institute of Molecular Physiology
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import typing
+
+import pandas as pd # type: ignore
+
+from . import util
+
+
+def get_cter_v1_0_header_names() -> typing.List[str]:
+    """
+    Returns the header names for the ctffind4 input file.
+
+    Arguments:
+    None
+
+    Returns:
+    List of names
+    """
+    return [
+        'defocus',
+        'cs',
+        'kv',
+        'pixel_size',
+        'b_factor',
+        'total_ac',
+        'astigmatism_amplitude',
+        'astigmatism_angle',
+        'std_defocus',
+        'std_total_ac',
+        'std_astigmatism_amplitude',
+        'std_astigmatism_angle',
+        'variation_defocus',
+        'variation_astigmatism_amplitude',
+        'resolution_limit_defocus',
+        'resolution_limit',
+        'nyquist',
+        'spare_1',
+        'spare_2',
+        'ac',
+        'phase_shift',
+        'micrograph_name'
+        ]
+
+
+def load_cter_v1_0(file_name: str) -> pd.DataFrame:
+    """
+    Load a ctffind file.
+
+    Arguments:
+    file_name - Path to the ctffind file
+
+    Returns:
+    Pandas dataframe containing the ctffind file information
+    """
+    header_names: typing.List[str]
+    cter_data: pd.DataFrame
+
+    header_names = get_cter_v1_0_header_names()
+    cter_data = util.load_file(
+        file_name,
+        names=header_names,
+        )
+    cter_data['ac'] = cter_data['ac'] / 100
+    cter_data['total_ac'] = cter_data['total_ac'] / 100
+    cter_data['astigmatism_angle'] = 45 - cter_data['astigmatism_angle']
+
+    defocus_data = pd.DataFrame(index=[0], columns=('defocus_u', 'defocus_v'))
+    defocus_data['defocus_u'], defocus_data['defocus_v'] = defocus_defocus_diff_to_defocus_u_and_v(
+        cter_data['defocus'],
+        cter_data['astigmatism_amplitude']
+        )
+    cter_data_dropped = cter_data.drop(labels=['defocus', 'astigmatism_amplitude'], axis=1)
+
+    return pd.concat([defocus_data, cter_data_dropped], axis=1)
+
+
+def defocus_defocus_diff_to_defocus_u_and_v(defocus, astigmatism):
+    """
+    Calculate the defocus_u and defocus_v value based on the average defocus
+    and the difference between the two values.
+
+    Arguments:
+    defocus - Average defocus value
+    astigmatism - Difference between the two defocus values (astigmatism amplitude)
+
+    Returns:
+    Defocus_U and Defocus_V
+    """
+    defocus_v = (20000*defocus - 10000*astigmatism) / 2
+    defocus_u = 20000*defocus - defocus_v
+    return defocus_u, defocus_v
---
+++ transphire_transform/dump_load/tests/test_cter.py
@@ -0,0 +1,144 @@
+"""
+MIT License
+
+Copyright (c) 2018 Max Planck Institute of Molecular Physiology
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from .. import cter
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+class TestGetCterV10HeaderNames:
+
+    def test_call_functions_should_return_filled_list(self):
+        data = [
+            'defocus',
+            'cs',
+            'kv',
+            'pixel_size',
+            'b_factor',
+            'total_ac',
+            'astigmatism_amplitude',
+            'astigmatism_angle',
+            'std_defocus',
+            'std_total_ac',
+            'std_astigmatism_amplitude',
+            'std_astigmatism_angle',
+            'variation_defocus',
+            'variation_astigmatism_amplitude',
+            'resolution_limit_defocus',
+            'resolution_limit',
+            'nyquist',
+            'spare_1',
+            'spare_2',
+            'ac',
+            'phase_shift',
+            'micrograph_name'
+            ]
+        assert cter.get_cter_v1_0_header_names() == data
+
+
+class TestLoadCterV10:
+
+    def test_correct_file_should_return_filled_data_frame(self):
+        file_name = os.path.join(THIS_DIR, 'cter_v1_0.txt')
+        return_frame = cter.load_cter_v1_0(file_name=file_name)
+
+        columns = (
+            'defocus_u',
+            'defocus_v',
+            'cs',
+            'kv',
+            'pixel_size',
+            'b_factor',
+            'total_ac',
+            'astigmatism_angle',
+            'std_defocus',
+            'std_total_ac',
+            'std_astigmatism_amplitude',
+            'std_astigmatism_angle',
+            'variation_defocus',
+            'variation_astigmatism_amplitude',
+            'resolution_limit_defocus',
+            'resolution_limit',
+            'nyquist',
+            'spare_1',
+            'spare_2',
+            'ac',
+            'phase_shift',
+            'micrograph_name'
+            )
+        data = [
+            22862.365,
+            22257.635,
+            0.01,
+            300,
+            1.14,
+            0,
+            0.1,
+            19.435,
+            0.0010212,
+            0,
+            0.0021005,
+            6.5849,
+            0.045268,
+            3.4734,
+            0.43346,
+            0.35979,
+            0.4386,
+            0.4386,
+            0,
+            0.1,
+            0,
+            'test_file.mrc'
+            ]
+        data_frame = pd.DataFrame(
+            [data],
+            columns=columns
+            )
+
+        assert data_frame.equals(return_frame.round(7))
+
+
+class TestDefocusDefocusDiffToDefocuUAndV:
+
+    def test_defocus_2_um_zero_astigmatism_should_return_20000_angstrom(self):
+        def_u, def_v = cter.defocus_defocus_diff_to_defocus_u_and_v(2, 0)
+        assert def_u == 20000
+
+    def test_zero_astigmatism_should_return_same_values(self):
+        def_u, def_v = cter.defocus_defocus_diff_to_defocus_u_and_v(2, 0)
+        assert def_u == def_v
+
+    def test_values_should_return_correct_defocus_u(self):
+        def_u, def_v = cter.defocus_defocus_diff_to_defocus_u_and_v(2.05, 0.1)
+        assert def_u == 21000
+
+    def test_values_should_return_correct_defocus_v(self):
+        def_u, def_v = cter.defocus_defocus_diff_to_defocus_u_and_v(2.05, 0.1)
+        assert def_v == 20000
