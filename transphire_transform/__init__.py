fe8678f536ae8961b7d1e18fd58fae48c7264285
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-10-10 15:34:29 +0200

    Add motioncor2 to __init__.py

--- transphire_transform/__init__.py
+++ transphire_transform/__init__.py
@@ -37,3 +37,5 @@ assert load_file
 assert dump_file
 from .dump_load.xml import load_xml # silence pyflakes
 assert load_xml
+from .dump_load.motioncor2 import load_motioncor2 # silence pyflakes
+assert load_motioncor2
